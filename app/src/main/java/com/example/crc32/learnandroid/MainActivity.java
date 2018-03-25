1aef0d65be233a2cd1fc0330ac9cab5ef8aeffc2
CRCx86 <crc32zinov@gmail.com>
2018-03-25 22:25:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/crc32/learnandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.crc32.learnandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
