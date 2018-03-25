bcd629da2fc85dc83aaaeca50b229d009f3a5519
CRCx86 <crc32zinov@gmail.com>
2018-03-25 23:08:54 +0300

    Commit 2 lesson changes

--- app/src/main/java/com/example/crc32/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/crc32/learnandroid/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.crc32.learnandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView mHellTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHellTextView = findViewById(R.id.textView);
+    }
+
+    public void onClick(View view) {
+        mHellTextView.setText("Hello Kitty!");
     }
 }
